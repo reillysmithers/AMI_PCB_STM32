cefa9c80a8d6ea3612e7232e7fb9fe4abc0b2eaf
reillysmithers <89340541+reillysmithers@users.noreply.github.com>
2024-08-23 09:05:12 +1000

    Added push button ioc stuff

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -337,6 +337,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PB7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
